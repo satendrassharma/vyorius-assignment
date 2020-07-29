1fde8174e47068adf625fa20b89a9f4c216e2aa3
Satendra sharma <satendrasharma6991@gmail.com>
2020-07-29 12:54:06 +0530

    assigment completed

---
+++ src/components/AboutSection.js
@@ -0,0 +1,34 @@
+import React from "react";
+import headerRight from "../assets/header-right.png";
+
+export default function AboutSection() {
+  return (
+    <section>
+      <div className="container">
+        <div className="left">
+          <h4 className="text-orange">CONNECTING THE DISCONNECTED</h4>
+          <h1>
+            <span className="text-orange">Vyorius</span> brings unmanned robots
+            <span className="text-orange">together</span>,<br />
+            wherever they are
+          </h1>
+          <p>
+            With all of the operations, commanding and maintenance tools in one
+            place, unmanned vehicles will stay connected and productive no
+            matter where you’re Delivering
+          </p>
+          <div className="buttons">
+            <button className="btn btn-primary btn-large">Try Vyorius</button>
+            <button className="btn btn-reverse btn-large">Learn More</button>
+          </div>
+          <p className="get-started">
+            Need to order a delivery? <a href="#">Get Started</a>
+          </p>
+        </div>
+        <div className="right">
+          <img src={headerRight} alt="about vyorius" />
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import logo from "../assets/logo.png";
+import brand from "../assets/brand.png";
+
+function Navbar() {
+  return (
+    <nav>
+      <div className="logo-container">
+        <img className="logo" src={logo} alt="logo" />
+        <img className="brand" src={brand} alt="brand" />
+      </div>
+      <ul className="links">
+        <li>
+          <a href="#">Why Vyorius?</a>
+        </li>
+        <li>
+          <a href="#">Solutions</a>
+        </li>
+        <li>
+          <a href="#">Product</a>
+        </li>
+        <li>
+          <a href="#">Use Cases</a>
+        </li>
+        <li>
+          <a href="#">Team</a>
+        </li>
+        <li>
+          <a href="#">Partners</a>
+        </li>
+        <li>
+          <a href="#">Contact us</a>
+        </li>
+      </ul>
+      <div className="btn-container">
+        <button className="btn btn-primary">Launch Vyorius &gt;</button>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
